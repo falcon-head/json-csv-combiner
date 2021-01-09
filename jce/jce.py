b9e89779606ec3a6b99ce647279b137fa8b13dae
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2021-01-09 19:30:06 +0530

    Addee the ability to merge multiple JSON file into one single file

--- jce/jce.py
+++ jce/jce.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import glob
 from tqdm import tqdm
+import json
 
 # All the functions go here
 
 def get_json_file_count():
-
     """
     Get the total numebr of JSON file present in the current directory
     """
@@ -16,8 +16,8 @@ def get_json_file_count():
 
     return number_of_json_file
 
-def get_csv_file_count():
 
+def get_csv_file_count():
     """
     Get the total number of CSV file present in the current directory
     """
@@ -28,8 +28,8 @@ def get_csv_file_count():
 
     return number_of_csv_file
 
-def get_excel_file_count(parameter_list):
 
+def get_excel_file_count(parameter_list):
     """
     Get the total number of CSV file present in the current directory
     """
@@ -42,7 +42,6 @@ def get_excel_file_count(parameter_list):
 
 
 def combine_excel(output_excel_name):
-
     """
     Combine all the excel file by reading
 
@@ -53,12 +52,12 @@ def combine_excel(output_excel_name):
     combined_excel_data = pd.DataFrame()
     for f in tqdm(glob.glob("*.xlsx")):
         df = pd.read_excel(f)
-        all_data = all_data.append(df,ignore_index=True)
+        all_data = all_data.append(df, ignore_index=True)
 
     combined_excel_data.to_excel(output_excel_name, index=False)
 
-def combine_csv(output_csv_name):
 
+def combine_csv(output_csv_name):
     """
     Combine all the excel file by reading
 
@@ -69,6 +68,24 @@ def combine_csv(output_csv_name):
     combined_csv_data = pd.DataFrame()
     for f in tqdm(glob.glob("*.csv")):
         df = pd.read_csv(f)
-        all_data = all_data.append(df,ignore_index=True)
+        all_data = all_data.append(df, ignore_index=True)
 
     combined_csv_data.to_csv(output_csv_name)
+
+
+def combine_csv(output_json_name):
+
+    """
+    Combine all the json file by reading
+
+    Args:
+        output_file_name (string): name of the output file
+    """
+
+    combined_json_data = []
+    for f in glob.glob("*.json"):
+        with open(f, "rb") as input_file:
+            combined_json_data.append(json.load(input_files))
+
+    with open(output_json_name+".json", "wb") as output_file:
+        json.dump(combined_json_data, output_file)
